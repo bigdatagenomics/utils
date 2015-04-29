e49006081c0e0fd45e2550e8312eb90ca190772f
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2015-04-29 12:16:13 -0700

    Cleaning up a small error in the release scripts.

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# what is the current revision?
-REVISION=git rev-parse HEAD
-
 # do scala 2.10 release
 mvn -Dresume=false release:clean release:prepare release:perform
 
 # do scala 2.11 release
-git checkout $REVISION
 ./scripts/move_to_scala_2.11.sh
 git commit -a -m "Modifying pom.xml files for 2.11 release."
 mvn -Dresume=false release:clean release:prepare release:perform
+
+# move back to 2.10 for development
+./scripts/move_to_scala_2.10.sh
+git commit -a -m "Modifying pom.xml files to move back to Scala 2.10 for development."
