adcc39ad71d5d9e6fff9d433de732145df2ba0f5
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2015-04-29 09:14:05 -0700

    Revert "[utils-17] Cross compile to scala 2.10 and 2.11."
    This reverts commit 47e325a6558b512108d5e695b33667cdd9d6ceae.

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 mvn -Dresume=false release:clean release:prepare release:perform
-
-mvn -Dresume=false release:clean release:prepare release:perform -P scala-2.11
--- scripts/release/rollback.sh
+++ scripts/release/rollback.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 mvn release:rollback
-
-mvn release:rollback -P scala-2.11
