161c9d73a7343cc74259c6aebf7b45f987721b25
Michael L Heuer <heuermh@acm.org>
2017-01-20 11:06:46 -0600

    Keep maven-javadoc-plugin at version 2.10.4

--- scripts/move_to_scala_2.11.sh
+++ scripts/move_to_scala_2.11.sh
@@ -3,4 +3,6 @@
 set +x
 
 find . -name "pom.xml" -exec sed -e "s/2.10.6/2.11.8/g" -e "s/2.10/2.11/g" -i .2.11.bak '{}' \;
-find . -name "*.2.11.bak" -exec rm {} \;
+# keep maven-javadoc-plugin at version 2.10.4
+find . -name "pom.xml" -exec sed -e "s/2.11.4/2.10.4/g" -i.2.11.2.bak '{}' \;
+find . -name "*.2.11.*bak" -exec rm {} \;
