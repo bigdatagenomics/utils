0c5074dccb66d822721be604f33895f1ffa6a17f
Michael L Heuer <heuermh@acm.org>
2017-01-20 11:00:07 -0600

    Update dependency and plugin versions

--- scripts/move_to_scala_2.11.sh
+++ scripts/move_to_scala_2.11.sh
@@ -2,7 +2,5 @@
 
 set +x
 
-find . -name "pom.xml" -exec sed -e "s/2.10.4/2.11.4/g" -e "s/2.10/2.11/g" -i .2.11.bak '{}' \;
-# keep maven-javadoc-plugin at version 2.10.3
-find . -name "pom.xml" -exec sed -e "s/2.11.3/2.10.3/g" -i .2.11.3.bak '{}' \;
+find . -name "pom.xml" -exec sed -e "s/2.10.6/2.11.8/g" -e "s/2.10/2.11/g" -i .2.11.bak '{}' \;
 find . -name "*.2.11.bak" -exec rm {} \;
