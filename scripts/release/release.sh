e36ef3f9d9aa966e56395cff63a838f808e8a36e
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2017-03-21 08:55:25 -0700

    Fix bad if clause in release scripts.

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # do we have enough arguments?
-if [ $# < 3 ]; then
+if [[ $# != 2 ]]; then
     echo "Usage:"
     echo
     echo "./release.sh <release version> <development version>"
