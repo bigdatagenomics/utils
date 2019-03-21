ac39fe22c4f8faaf0b92a327db097490427f5869
Michael L Heuer <heuermh@acm.org>
2019-03-21 11:51:47 -0500

    Adding github-changes-maven-plugin configuration to release script.

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 # do we have enough arguments?
-if [[ $# != 2 ]]; then
+if [[ $# < 4 ]]; then
     echo "Usage:"
     echo
-    echo "./release.sh <release version> <development version>"
+    echo "./release.sh <release version> <development version> <milestone id>"
     exit 1
 fi
 
 # pick arguments
 release=$1
 devel=$2
+milestone=$3
 
 # get current branch
 branch=$(git status -bs | awk '{ print $2 }' | awk -F'.' '{ print $1 }' | head -n 1)
+
+# update changelog per Github milestone
+mvn com.github.heuermh.maven.plugin.changes:github-changes-maven-plugin:1.0:github-changes -DmilestoneId=${milestone}
+git commit -a -m "Modifying changelog."
 
 commit=$(git log --pretty=format:"%H" | head -n 1)
 echo "releasing from ${commit} on branch ${branch}"
