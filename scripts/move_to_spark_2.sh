ff3c69213a343770e184a0f0ce0bad709d2610fc
Michael L Heuer <heuermh@acm.org>
2020-02-10 13:15:15 -0600

    Add move to Spark 2/3 scripts.

---
+++ scripts/move_to_spark_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set +x
+
+grep -q "spark2" pom.xml
+if [[ $? == 0 ]];
+then
+    echo "POM is already set up for Spark 2 (Spark 2 artifacts have -spark2 suffix in artifact names)."
+    echo "Cowardly refusing to move to Spark 2 a second time..."
+
+    exit 1
+fi
+
+svp="\${scala.version.prefix}"
+substitution_cmd="s/-spark3_$svp/-spark2_$svp/g"
+
+find . -name "pom.xml" -exec sed \
+    -e "/utils-/ s/-spark3_2\.1/-spark2_2\.1/" \
+    -e "/utils-/ $substitution_cmd" \
+    -e "/spark.version/ s/3.0.0-preview2/2.4.5/g" \
+    -i.spark2.bak '{}' \;
